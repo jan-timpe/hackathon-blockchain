79800a42c2899ca1d0512723a51f34268ebce2ea
tnakka214 <tnakka@email.uark.edu>
2018-03-07 15:23:28 -0600

    Revert "Added dot inför test cases"
    This reverts commit b670269bb15635636bf7cd305f606d04400ef91d.

--- contracts/CarrierDotInfo.sol
+++ contracts/CarrierDotInfo.sol
@@ -6,13 +6,13 @@ contract CarrierDotInfo {
     // "dot census" -- dot licensing and insurance can be another contract
 
     address public managingContract;
-    string public usDotNumber;
-    string public dbaName;
-    string public legalName;
-    string public streetAddress;
-    string public streetPhone;
-    string public mailingAddress;
-    string public mailingPhone;
+    bytes32 public usDotNumber;
+    bytes32 public dbaName;
+    bytes32 public legalName;
+    bytes32 public streetAddress;
+    bytes32 public streetPhone;
+    bytes32 public mailingAddress;
+    bytes32 public mailingPhone;
 
     function CarrierDotInfo() public {
         managingContract = msg.sender;
@@ -21,50 +21,50 @@ contract CarrierDotInfo {
     function getManagingContract() public view returns (address) {
         return managingContract;
     }
-    function getUsDotNumber() public view returns (string) {
+    function getUsDotNumber() public view returns (bytes32) {
         return usDotNumber;
     }
-    function getDbaName() public view returns (string) {
+    function getDbaName() public view returns (bytes32) {
         return dbaName;
     }
-    function getLegalName() public view returns (string) {
+    function getLegalName() public view returns (bytes32) {
         return legalName;
     }
-    function getStreetAddress() public view returns (string) {
+    function getStreetAddress() public view returns (bytes32) {
         return streetAddress;
     }
-    function getStreetPhone() public view returns (string) {
+    function getStreetPhone() public view returns (bytes32) {
         return streetPhone;
     }
-    function getMailingAddress() public view returns (string) {
+    function getMailingAddress() public view returns (bytes32) {
         return mailingAddress;
     }
-    function getMailingPhone() public view returns (string) {
+    function getMailingPhone() public view returns (bytes32) {
         return mailingPhone;
     }
 
     function setManagingContract (address _managingContract) public {
         managingContract = _managingContract;
     }
-    function setUsDotNumber (string _usDotNumber) public {
+    function setUsDontNumber (bytes32 _usDotNumber) public {
         usDotNumber = _usDotNumber;
     }
-    function setDbaName (string _dbaName) public {
+    function setDbaName (bytes32 _dbaName) public {
         dbaName = _dbaName;
     }
-    function setLegalName (string _legalName) public {
+    function setLegalName (bytes32 _legalName) public {
         legalName = _legalName;
     }
-    function setStreetAddress (string _streetAddress) public {
+    function setStreetAddress (bytes32 _streetAddress) public {
         streetAddress = _streetAddress;
     }
-    function setStreetPhone (string _streetPhone) public {
+    function setStreetPhone (bytes32 _streetPhone) public {
         streetPhone = _streetPhone;
     }
-    function setMailingAddress (string _mailingAddress) public {
+    function setMailingAddress (bytes32 _mailingAddress) public {
         mailingAddress = _mailingAddress;
     }
-    function setMailingPhone (string _mailingPhone) public {
+    function setMailingPhone (bytes32 _mailingPhone) public {
         mailingPhone = _mailingPhone;
     }
 
