e72a5c778a5738a588447663320172c6dbd7d80e
jantimpe <timpe31@gmail.com>
2018-03-07 13:33:53 -0600

    added getters, setters

--- contracts/CarrierContact.sol
+++ contracts/CarrierContact.sol
@@ -20,4 +20,95 @@ contract CarrierContact {
 
 
 
+    function set_firstName(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        firstName = _val;
+    }
+
+    function get_firstName() public view returns (bytes32) {
+        return firstName;
+    }
+
+
+
+    function set_lastName(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        lastName = _val;
+    }
+
+    function get_lastName() public view returns (bytes32) {
+        return lastName;
+    }
+
+
+
+    function set_jobTitle(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        jobTitle = _val;
+    }
+
+    function get_jobTitle() public view returns (bytes32) {
+        return jobTitle;
+    }
+
+
+
+    function set_contactType(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        contactType = _val;
+    }
+
+    function get_contactType() public view returns (bytes32) {
+        return contactType;
+    }
+
+
+
+    function set_phone(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        phone = _val;
+    }
+
+    function get_phone() public view returns (bytes32) {
+        return phone;
+    }
+
+
+
+    function set_mobilePhone(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        mobilePhone = _val;
+    }
+
+    function get_mobilePhone() public view returns (bytes32) {
+        return mobilePhone;
+    }
+
+
+
+    function set_fax(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        fax = _val;
+    }
+
+    function get_fax() public view returns (bytes32) {
+        return fax;
+    }
+
+
+
+    function set_email(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        email = _val;
+    }
+
+    function get_email() public view returns (bytes32) {
+        return email;
+    }
+
+
+
+
+
+
 }
--- contracts/CarrierDotInfo.sol
+++ contracts/CarrierDotInfo.sol
@@ -20,4 +20,75 @@ contract CarrierDotInfo {
         managingContract = msg.sender;
     }
 
+    function set_usDotNumber(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        usDotNumber = _val;
+    }
+
+    function get_usDotNumber() public view returns (bytes32) {
+        return usDotNumber;
+    }
+
+
+    function set_dbaName(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        dbaName = _val;
+    }
+
+    function get_dbaName() public view returns (bytes32) {
+        return dbaName;
+    }
+
+
+    function set_legalName(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        legalName = _val;
+    }
+
+    function get_legalName() public view returns (bytes32) {
+        return legalName;
+    }
+
+
+    function set_streetAddress(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        streetAddress = _val;
+    }
+
+    function get_streetAddress() public view returns (bytes32) {
+        return streetAddress;
+    }
+
+
+    function set_streetPhone(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        streetPhone = _val;
+    }
+
+    function get_streetPhone() public view returns (bytes32) {
+        return streetPhone;
+    }
+
+
+    function set_mailingAddress(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        mailingAddress = _val;
+    }
+
+    function get_mailingAddress() public view returns (bytes32) {
+        return mailingAddress;
+    }
+
+
+    function set_mailingPhone(bytes32 _val) public {
+        require(msg.sender == managingContract);
+        mailingPhone = _val;
+    }
+
+    function get_mailingPhone() public view returns (bytes32) {
+        return mailingPhone;
+    }
+
+
+
 }
